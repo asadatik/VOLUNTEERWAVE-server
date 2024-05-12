ac8e1fe3439c03d2479ef8d14e80a34a038ec00c
Asad Atik <asadatik1995@gmail.com>
2024-05-12 19:24:15 +0600

    Post Oparetion Success

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ const corsOptions = {
   }
   app.use(cors(corsOptions))
   app.use(express.json())
-
-       
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASs}@cluster0.ldjypij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,14 +31,34 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // collections
+    const PostCollection = client.db('Volunteers').collection('post')
+
+
+
+ // Save a job data in db
+    app.post('/AddPost', async (req, res) => {
+        const jobData = req.body
+  
+        const result = await PostCollection.insertOne(jobData)
+        res.send(result)
+      })    
+        // Get all Posed data from db
+        app.get('/AddPost', async (req, res) => {
+            const result = await PostCollection.find().toArray()
+      
+            res.send(result)
+          })
+    
+
+
+
+
+
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
