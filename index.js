6ba4c774149cf11331e574cec87e973d895cfc31
Asad Atik <asadatik1995@gmail.com>
2024-05-13 03:28:58 +0600

    Get a single Add Post  data from collection using  id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
@@ -50,6 +50,13 @@ async function run() {
             res.send(result)
           })
     
+    // Get a single Add Post  data from collection using  id
+    app.get('/post/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await PostCollection.findOne(query)
+      res.send(result)
+    })
 
 
 
@@ -62,11 +69,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-
-
 
 
 app.get('/', (req, res) => {
